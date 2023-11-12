0e35648437c9e28a7158ac368c4b4d134d382929
finik9808@gmail.com <finik9808@gmail.com>
2023-11-12 23:21:09 +0700

    Добавьте файлы проекта.

---
+++ Parser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using LB6.Models;
+
+namespace LB6
+{
+    class Parser
+    {
+        public static async Task Parse(string url)
+        {
+            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+            var doc = await context.OpenAsync(url);
+            var aList = doc.QuerySelectorAll("div.product-preview__title a").Select(elem => doc.Origin + elem.GetAttribute("href"));
+            NoteBook nb;
+            IElement value;
+            foreach ( var a in aList )
+            {
+                Console.WriteLine(a);
+                doc = await context.OpenAsync(a);
+                nb = new NoteBook();
+                nb.Id = Guid.NewGuid();
+                value = doc.QuerySelector("div.product__area-title > h1.product__title");
+                nb.Name = value.TextContent;
+                foreach (var prop in doc.QuerySelectorAll("div#tab-characteristics div.property")
+                    .Select(prop => new KeyValuePair<string, string>(prop.QuerySelector("div.property__name").TextContent, prop.QuerySelector("div.property__content").TextContent)))
+                {
+                    if (prop.Key == "Бренд")
+                        nb.Brand = ClearValue(prop.Value);
+                    else if (prop.Key.Contains("Частота обновления экрана"))
+                        nb.Frequency = ClearValue(prop.Value);
+                    else if (prop.Key.Contains("Разрешение экрана"))
+                        nb.Resolution = ClearValue(prop.Value);
+                    else if (prop.Key.Contains("Вес"))
+                        nb.Weight = ClearValue(prop.Value);
+                }
+                SiteParseEntities db = new SiteParseEntities();
+                db.NoteBook.Add(nb);
+                db.SaveChanges();
+            }
+        }
+        private static string ClearValue(string value)
+        {
+            return value.Trim(' ', '\n');
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using LB6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB6
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            await Parser.Parse(@"https://2droida.ru/collection/noutbuki-xiaomi");
+            var dbcon = new SiteParseEntities();
+            foreach (var n in dbcon.NoteBook)
+                Console.WriteLine("{0}\n\t{1}\n\t{2}\n\t{3}", n.Name, n.Brand, n.Frequency, n.Resolution, n.Weight);
+            Console.ReadLine();
+        }
+    }
+}
